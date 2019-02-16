de5d6ea29dfebf3459078b461d7e957b63319985
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-16 12:18:51 -0300

    Class abstraction method was added

--- app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactMVP.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactMVP.java
@@ -1,16 +1,30 @@
 package phenrique.picpay.desafiopicpay.mvp.contact;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import phenrique.picpay.desafiopicpay.data.model.User;
+
 public interface ContactMVP {
 
     interface ViewImpl {
-
+        public void showToast( String mensagem );
+        public void showProgressBar( int visibilidade );
+        public void updateListRecycler();
     }
 
     interface PresenterImpl {
-
+        public void showToast( String mensagem );
+        public void showProgressBar( boolean status );
+        public void setView( ContactMVP.ViewImpl view );
+        public Context getContext();
+        public ArrayList<User> getUsers();
+        public void retrieveUsers();
+        public void updateListRecycler(ArrayList<User> users);
     }
 
     interface ModelImpl {
-
+        public void retrieveUsers();
     }
 }
