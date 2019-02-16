268e97518c41c4c988c746ba0cf3b355ca932c6e
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-16 12:20:52 -0300

    Added methods for searching contacts

--- app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactModel.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactModel.java
@@ -1,10 +1,54 @@
 package phenrique.picpay.desafiopicpay.mvp.contact;
 
+import android.util.Log;
+
+import phenrique.picpay.desafiopicpay.data.model.User;
+import phenrique.picpay.desafiopicpay.data.model.UserResults;
+import phenrique.picpay.desafiopicpay.data.network.ApiManager;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
 public class ContactModel implements ContactMVP.ModelImpl {
 
     private ContactMVP.PresenterImpl presenter;
 
     public ContactModel(ContactMVP.PresenterImpl presenter) {
         this.presenter = presenter;
+    }
+
+    @Override
+    public void retrieveUsers() {
+        try {
+
+            presenter.showProgressBar(true);
+
+            Call<UserResults> call = ApiManager.get().searchUsers();
+
+            call.enqueue(new Callback<UserResults>() {
+                @Override
+                public void onFailure(Call<UserResults> call, Throwable t) {
+                    t.printStackTrace();
+                    presenter.showToast( t.getMessage() );
+                }
+
+                @Override
+                public void onResponse(Call<UserResults> call, Response<UserResults> response) {
+                    UserResults contacts = response.body();
+                    Log.d("APIPlug", "Successfully response fetched");
+
+                    presenter.showProgressBar(false);
+
+                    if (contacts.results.size() > 0) {
+                        presenter.updateListRecycler(contacts.results);
+                    }
+                }
+
+            });
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
