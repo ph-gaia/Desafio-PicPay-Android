1ad92232b59c8e90df570d9ed80d67167f980c8b
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-18 22:15:17 -0300

    added a filterable on adapter

--- app/src/main/java/phenrique/picpay/desafiopicpay/adapter/ContactAdapter.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/adapter/ContactAdapter.java
@@ -5,19 +5,57 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import phenrique.picpay.desafiopicpay.R;
 import phenrique.picpay.desafiopicpay.data.model.User;
 
-public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHolder> {
+public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHolder> implements Filterable {
 
     private List<User> usersList;
+    private List<User> usersListFiltered;
+    private UserAdapterListener listener;
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    usersListFiltered = usersList;
+                } else {
+                    List<User> filteredList = new ArrayList<>();
+                    for (User row : usersList) {
+                        if (row.getName().toLowerCase().contains(charString.toLowerCase()) ||
+                                row.getUsername().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+
+                    usersListFiltered = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = usersListFiltered;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                usersListFiltered = (ArrayList<User>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public TextView username, name;
@@ -31,8 +69,10 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHo
         }
     }
 
-    public ContactAdapter(List<User> usersList) {
+    public ContactAdapter(List<User> usersList, UserAdapterListener listener) {
         this.usersList = usersList;
+        this.usersListFiltered = usersList;
+        this.listener = listener;
     }
 
     @Override
@@ -58,5 +98,9 @@ public class ContactAdapter extends RecyclerView.Adapter<ContactAdapter.MyViewHo
     @Override
     public int getItemCount() {
         return usersList.size();
+    }
+
+    public interface UserAdapterListener {
+        void onUserSelected(User user);
     }
 }
