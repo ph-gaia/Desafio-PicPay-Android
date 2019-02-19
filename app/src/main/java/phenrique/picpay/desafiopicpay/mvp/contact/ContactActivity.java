7fb0c531e1507b0220bb44a88ea33c6a0a567e55
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-18 22:17:19 -0300

    Added filter instance in adapter class

--- app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactActivity.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactActivity.java
@@ -4,30 +4,27 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.widget.EditText;
-import android.widget.ProgressBar;
+import android.support.v7.widget.SearchView;
 import android.widget.Toast;
 
 import phenrique.picpay.desafiopicpay.R;
 import phenrique.picpay.desafiopicpay.adapter.ContactAdapter;
+import phenrique.picpay.desafiopicpay.data.model.User;
 
-public class ContactActivity extends AppCompatActivity implements ContactMVP.ViewImpl {
+public class ContactActivity extends AppCompatActivity implements ContactMVP.ViewImpl,
+        ContactAdapter.UserAdapterListener {
 
     private static ContactMVP.PresenterImpl presenter;
 
     private ContactAdapter adapter;
 
-    private EditText contact_search;
     private RecyclerView contact_list;
+    private SearchView contact_search;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_list);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
         if( presenter == null ) {
             presenter = new ContactPresenter();
@@ -44,9 +41,28 @@ public class ContactActivity extends AppCompatActivity implements ContactMVP.Vie
         contact_list = findViewById(R.id.contact_list);
         contact_list.setHasFixedSize(true);
 
-        adapter = new ContactAdapter(presenter.getUsers());
+        adapter = new ContactAdapter(presenter.getUsers(),this);
         contact_list.setAdapter(adapter);
 
+        searchListener();
+    }
+
+    private void searchListener() {
+        contact_search = findViewById(R.id.contact_search);
+
+        contact_search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                adapter.getFilter().filter(query);
+                return false;
+            }
+        });
     }
 
     public void updateListRecycler() {
@@ -59,5 +75,10 @@ public class ContactActivity extends AppCompatActivity implements ContactMVP.Vie
 
     public void showProgressBar( int visibilidade ) {
         findViewById(R.id.contact_loading_screen).setVisibility( visibilidade );
+    }
+
+    @Override
+    public void onUserSelected(User user) {
+        Toast.makeText(getApplicationContext(), "Selected: " + user.getName(), Toast.LENGTH_LONG).show();
     }
 }
