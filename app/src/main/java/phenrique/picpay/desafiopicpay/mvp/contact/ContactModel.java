307360151299e65630971cb1bbb24e5d6f851534
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-19 23:19:02 -0300

    Fix small bugs

--- app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactModel.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/mvp/contact/ContactModel.java
@@ -1,9 +1,8 @@
 package phenrique.picpay.desafiopicpay.mvp.contact;
 
-import android.util.Log;
+import java.util.List;
 
 import phenrique.picpay.desafiopicpay.data.model.User;
-import phenrique.picpay.desafiopicpay.data.model.UserResults;
 import phenrique.picpay.desafiopicpay.data.network.ApiManager;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -23,31 +22,30 @@ public class ContactModel implements ContactMVP.ModelImpl {
 
             presenter.showProgressBar(true);
 
-            Call<UserResults> call = ApiManager.get().searchUsers();
+            Call<List<User>> call = ApiManager.get().searchUsers();
 
-            call.enqueue(new Callback<UserResults>() {
+            call.enqueue(new Callback<List<User>>() {
                 @Override
-                public void onFailure(Call<UserResults> call, Throwable t) {
+                public void onFailure(Call<List<User>> call, Throwable t) {
                     t.printStackTrace();
+                    presenter.showProgressBar(false);
                     presenter.showToast( t.getMessage() );
                 }
 
                 @Override
-                public void onResponse(Call<UserResults> call, Response<UserResults> response) {
-                    UserResults contacts = response.body();
-                    Log.d("APIPlug", "Successfully response fetched");
+                public void onResponse(Call<List<User>> call, Response<List<User>> response) {
+                    List<User> contacts = response.body();
 
                     presenter.showProgressBar(false);
 
-                    if (contacts.results.size() > 0) {
-                        presenter.updateListRecycler(contacts.results);
-                    }
+                    presenter.updateListRecycler(contacts);
                 }
 
             });
 
         } catch (Exception e) {
             e.printStackTrace();
+            presenter.showProgressBar(false);
         }
 
     }
