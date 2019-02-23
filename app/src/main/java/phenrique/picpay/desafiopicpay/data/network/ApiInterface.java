a6be882d59c639be5054b5369b757b181f998f63
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-22 23:51:08 -0300

    Added interface to resquest transaction

--- app/src/main/java/phenrique/picpay/desafiopicpay/data/network/ApiInterface.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/data/network/ApiInterface.java
@@ -2,13 +2,19 @@ package phenrique.picpay.desafiopicpay.data.network;
 
 import java.util.List;
 
+import phenrique.picpay.desafiopicpay.data.model.Payment;
+import phenrique.picpay.desafiopicpay.data.model.Transaction;
 import phenrique.picpay.desafiopicpay.data.model.User;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface ApiInterface {
 
     @GET("users")
     Call<List<User>> searchUsers();
 
+    @POST("transaction")
+    Call<Transaction> sendTransaction(@Body Payment payment);
 }
