cf6dd9868e9af40ae643ced05c452046f9fd02b0
Paulo Henrique <phcgaia11@yahoo.com.br>
2019-02-24 15:26:04 -0300

    Added method interface MVP

--- app/src/main/java/phenrique/picpay/desafiopicpay/mvp/payment/PaymentMVP.java
+++ app/src/main/java/phenrique/picpay/desafiopicpay/mvp/payment/PaymentMVP.java
@@ -1,4 +1,29 @@
 package phenrique.picpay.desafiopicpay.mvp.payment;
 
+import android.content.Context;
+import android.widget.EditText;
+
+import phenrique.picpay.desafiopicpay.data.model.User;
+
 public interface PaymentMVP {
+
+    interface ViewImpl {
+        public void showToast( String mensagem );
+        public void showProgressBar( int visibilidade );
+        public void populateView(User user);
+        public void setTransactionValue(String value, int color, boolean enableButton);
+    }
+
+    interface PresenterImpl {
+        public void showToast( String mensagem );
+        public void showProgressBar( boolean status );
+        public void setView( PaymentMVP.ViewImpl view );
+        public Context getContext();
+        public void transactionValuesListener(String text);
+        public void initSettings();
+    }
+
+    interface ModelImpl {
+
+    }
 }
